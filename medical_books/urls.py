561c70e616d564132183e382a666ef9f3963c571
gayatriraj123 <gayatrirajguru2002@gmail.com>
2025-03-20 09:38:42 +0530

    createSuperAdmin

--- medical_books/urls.py
+++ medical_books/urls.py
@@ -20,6 +20,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
-    path('api/books', include('books.urls')),
+    path('api/books/', include('books.urls')),
     path('api/', include('assessments.urls')),
 ]
+
+
