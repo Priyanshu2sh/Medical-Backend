40511d5044b4779da3789d7f9d45bba61b78a1d0
gayatriraj123 <gayatrirajguru2002@gmail.com>
2025-04-29 15:07:40 +0530

    test

--- medical_books/middleware.py
+++ medical_books/middleware.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponseBadRequest
 
 class SanitizeHostMiddleware:
-    def __init__(self, get_response):
+    def _init_(self, get_response):
         self.get_response = get_response
 
-    def __call__(self, request):
+    def _call_(self, request):
         host = request.get_host().split(',')[0].strip()  # Take first domain if duplicated
         request.META['HTTP_HOST'] = host
         return self.get_response(request)
--- medical_books/settings.py
+++ medical_books/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'medical_books.middleware.SanitizeHostMiddleware',
+    'medical_books.SanitizeHostMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
